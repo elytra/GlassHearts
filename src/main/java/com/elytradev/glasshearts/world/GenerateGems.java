de4eefad6cc04c5529a850e4ed7109cc2c4fef53
Aesen Vismea <aesen@unascribed.com>
2017-04-25 16:15:42 -0400

    Implement petrified trees

--- src/main/java/com/elytradev/glasshearts/world/GenerateGems.java
+++ src/main/java/com/elytradev/glasshearts/world/GenerateGems.java
@@ -6,6 +6,7 @@ import com.elytradev.glasshearts.GlassHearts;
 import com.elytradev.glasshearts.block.BlockOre;
 import com.elytradev.glasshearts.enums.EnumGem;
 
+import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkGenerator;
@@ -16,12 +17,16 @@ public class GenerateGems implements IWorldGenerator {
 
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
+		if (!world.provider.isSurfaceWorld()) return;
 		for (int i = 0; i < 12; i++) {
 			int x = (chunkX*16)+random.nextInt(16);
 			int y = random.nextInt(24)+3;
 			int z = (chunkZ*16)+random.nextInt(16);
-			EnumGem gem = BlockOre.VALID_GEMS[random.nextInt(BlockOre.VALID_GEMS.length)];
-			world.setBlockState(new BlockPos(x, y, z), GlassHearts.inst.ORE.getDefaultState().withProperty(BlockOre.VARIANT, gem));
+			BlockPos pos = new BlockPos(x, y, z);
+			if (world.getBlockState(pos).getBlock() == Blocks.STONE) {
+				EnumGem gem = BlockOre.VALID_GEMS[random.nextInt(BlockOre.VALID_GEMS.length)];
+				world.setBlockState(pos, GlassHearts.inst.ORE.getDefaultState().withProperty(BlockOre.VARIANT, gem));
+			}
 		}
 	}
 
