f78efde0fdf09947122e3e7ee1362a2cca8f8e39
William Thompson <aesen@unascribed.com>
2017-05-21 21:53:37 -0400

    Add {un,}attune animations.

--- src/main/java/com/elytradev/glasshearts/client/guiparticle/GuiParticleHeartFragment.java
+++ src/main/java/com/elytradev/glasshearts/client/guiparticle/GuiParticleHeartFragment.java
@@ -7,6 +7,7 @@ import com.elytradev.glasshearts.client.HeartRenderer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
 
 public class GuiParticleHeartFragment extends GuiParticle {
 
@@ -21,7 +22,9 @@ public class GuiParticleHeartFragment extends GuiParticle {
 	private int textureWidth;
 	private int textureHeight;
 	
-	public GuiParticleHeartFragment(ResourceLocation texture, double x, double y, float u, float v, float w, float h, int textureWidth, int textureHeight) {
+	private boolean fade;
+	
+	public GuiParticleHeartFragment(ResourceLocation texture, double x, double y, float u, float v, float w, float h, int textureWidth, int textureHeight, boolean fade) {
 		super(x, y);
 		this.texture = texture;
 		
@@ -34,10 +37,18 @@ public class GuiParticleHeartFragment extends GuiParticle {
 		this.textureWidth = textureWidth;
 		this.textureHeight = textureHeight;
 		
-		motionX = rand.nextGaussian()*0.6;
-		motionY = -1;
+		this.fade = fade;
 		
-		maxAge = Integer.MAX_VALUE;
+		if (fade) {
+			motionX = 0;
+			motionY = 0;
+			maxAge = 10;
+			gravity = 0;
+		} else {
+			motionX = rand.nextGaussian()*0.6;
+			motionY = -1;
+			maxAge = Integer.MAX_VALUE;
+		}
 	}
 	
 	@Override
@@ -46,20 +57,32 @@ public class GuiParticleHeartFragment extends GuiParticle {
 		float interpPosY = (float)(prevPosY + (posY - prevPosY) * partialTicks);
 		
 		Minecraft.getMinecraft().getTextureManager().bindTexture(texture);
-		GlStateManager.color(red, green, blue);
+		float alpha = 1;
+		if (fade) {
+			alpha = 1-(MathHelper.sin((age/(float)maxAge)*(float)(Math.PI/2)));
+		}
+		GlStateManager.color(red, green, blue, alpha);
 		HeartRenderer.drawModalRectWithCustomSizedTexture(interpPosX, interpPosY, u, v, w, h, textureWidth, textureHeight);
 	}
 
-	public static void spawn(List<GuiParticle> guiParticles, ResourceLocation texture, int x, int y, float u, float v, float width, float height, int textureWidth, int textureHeight, float particleWidth, float particleHeight, float r, float g, float b) {
+	public static void spawnShatter(List<GuiParticle> guiParticles, ResourceLocation texture, int x, int y, float u, float v, float width, float height, int textureWidth, int textureHeight, float particleWidth, float particleHeight, float r, float g, float b) {
 		float divW = (width/particleWidth);
 		float divH = (height/particleHeight);
 		for (int i = 0; i < divW*divH; i++) {
-			GuiParticleHeartFragment p = new GuiParticleHeartFragment(texture, x+((i%divW)*particleWidth), y+((i/divW)*particleHeight), u+((i%divW)*particleWidth), v+((i/divW)*particleHeight), particleWidth, particleHeight, textureWidth, textureHeight);
+			GuiParticleHeartFragment p = new GuiParticleHeartFragment(texture, x+((i%divW)*particleWidth), y+((i/divW)*particleHeight), u+((i%divW)*particleWidth), v+((i/divW)*particleHeight), particleWidth, particleHeight, textureWidth, textureHeight, false);
 			p.red = r;
 			p.green = g;
 			p.blue = b;
 			guiParticles.add(p);
 		}
+	}
+	
+	public static void spawnFade(List<GuiParticle> guiParticles, ResourceLocation texture, int x, int y, float u, float v, float width, float height, int textureWidth, int textureHeight, float r, float g, float b) {
+		GuiParticleHeartFragment p = new GuiParticleHeartFragment(texture, x, y, u, v, width, height, textureWidth, textureHeight, true);
+		p.red = r;
+		p.green = g;
+		p.blue = b;
+		guiParticles.add(p);
 	}
 
 }
